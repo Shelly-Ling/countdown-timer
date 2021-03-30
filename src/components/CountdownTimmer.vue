ff5fb7594a4eaf505654cbdc8300702f406b1445
Shelly-Ling <sontee.ling.chen@gmail.com>
2021-03-30 21:39:46 +0800

    set countdown function

--- src/components/CountdownTimmer.vue
+++ src/components/CountdownTimmer.vue
@@ -3,24 +3,91 @@
     <h1>Countdown Clock</h1>
     <div id="clockdiv">
       <div class="time-card">
-        <span class="days"></span>
+        <div class="days time">
+          <span>{{ days }}</span>
+        </div>
         <div class="text">Days</div>
       </div>
       <div class="time-card">
-        <span class="hours"></span>
+        <div class="hours time">
+          <span>{{ hours }}</span>
+        </div>
         <div class="text">Hours</div>
       </div>
       <div class="time-card">
-        <span class="minutes"></span>
+        <div class="minutes time">
+          <span>{{ minutes }}</span>
+        </div>
         <div class="text">Minutes</div>
       </div>
       <div class="time-card">
-        <span class="seconds"></span>
+        <div class="seconds time">
+          <span>{{ seconds }}</span>
+        </div>
         <div class="text">Seconds</div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      endtime: {},
+      seconds: -1,
+      minutes: -1,
+      hours: -1,
+      days: -1,
+    }
+  },
+  created() {
+    this.getDeadLine()
+  },
+  mounted() {
+    this.getTimeRemaining(this.endtime)
+    this.updateClock()
+  },
+  methods: {
+    getDeadLine() {
+      this.endtime = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000)
+    },
+    getTimeRemaining(endtime) {
+      const total = Date.parse(endtime) - Date.parse(new Date())
+      const seconds = Math.floor((total / 1000) % 60)
+      const minutes = Math.floor((total / 1000 / 60) % 60)
+      const hours = Math.floor((total / (1000 * 60 * 60)) % 24)
+      const days = Math.floor(total / (1000 * 60 * 60 * 24))
+      return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds
+      }
+    },
+    updateClock() {
+      const t = this.getTimeRemaining(this.endtime);
+
+      this.days = t.days;
+      this.hours = ('0' + t.hours).slice(-2)
+      this.minutes = ('0' + t.minutes).slice(-2)
+      this.seconds = ('0' + t.seconds).slice(-2)
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval)
+      }
+
+      const timeinterval = setInterval(this.updateClock, 1000);
+      this.deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000);
+
+    },
+    initializeClock() {
+      this.initializeClock('clockdiv', this.deadline);
+    }
+  }
+}
+</script>>
 
 <style scoped>
 body {
@@ -38,6 +105,7 @@ body {
   width: 150px;
   height: 200px;
   margin: 15px;
+  text-align: center;
 }
 h1 {
   color: #396;
@@ -61,11 +129,20 @@ h1 {
   display: inline-block;
 }
 #clockdiv div > span {
-  width: 90%;
-  height: 80%;
+  padding: 40px;
   border-radius: 5px;
   background: #00816a;
   display: inline-block;
+  text-align: center;
+  font-size: 60px;
+  width: auto;
+  height: auto;
+}
+.time {
+  width: 101%;
+  height: 82%;
+  background: #00816a;
+  border-radius: 5px;
 }
 .text {
   font-size: 30px;
